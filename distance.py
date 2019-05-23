39a1e4da938dfc88c74cd8f9b28ddb83fb220149
NabiAbdHadi <nabil.hadi69@gmail.com>
2019-05-23 17:36:13 +0800

    the function to find the coordinate of the major city has been added

--- distance.py
+++ distance.py
@@ -3,14 +3,16 @@ from geopy import *
 
 
 def coordinate(input):
-    major_city_address = []
+    major_city_address = {}
     geolocator = Nominatim(user_agent="Best Flight")
     for index, city in enumerate(input):
         location = geolocator.geocode(city, language='en')
-        major_city_address.append((city, location.latitude, location.longitude))
-        gmap = gmplot.GoogleMapPlotter.from_geocode(city)
-        print(gmap)
+        major_city_address[city] = [location.latitude, location.longitude]
+
+        #gmap = gmplot.GoogleMapPlotter.from_geocode(major_city_address[city])
+        #print(gmap)
     return major_city_address
 
-major_city = ["New York City", "Istanbul", "Beijing", "Manila", "Berlin"]
+# Kuala lumpur as the reference place
+major_city = ["Kuala Lumpur", "New York City", "Istanbul", "Beijing", "Manila", "Berlin"]
 print(coordinate(major_city))
