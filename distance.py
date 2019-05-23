b996fc559932e9726dcf0cd74a34f7f77211aa32
NabiAbdHadi <nabil.hadi69@gmail.com>
2019-05-23 18:01:49 +0800

    the function for plot in google map has been added

--- distance.py
+++ distance.py
@@ -1,18 +1,25 @@
 import gmplot
 from geopy import *
 
-
 def coordinate(input):
     major_city_address = {}
     geolocator = Nominatim(user_agent="Best Flight")
     for index, city in enumerate(input):
         location = geolocator.geocode(city, language='en')
         major_city_address[city] = [location.latitude, location.longitude]
-
-        #gmap = gmplot.GoogleMapPlotter.from_geocode(major_city_address[city])
-        #print(gmap)
     return major_city_address
+
+#plot map using gmplot
+def plotMap(input):
+    for key,  value in input.items():
+        print(key, value)
+        gmap = gmplot.GoogleMapPlotter(value[0], value[1], 18)
+        gmap.draw("map%s.html" %key)
 
 # Kuala lumpur as the reference place
 major_city = ["Kuala Lumpur", "New York City", "Istanbul", "Beijing", "Manila", "Berlin"]
-print(coordinate(major_city))
+
+coor = coordinate(major_city)
+
+plotMap(coor)
+
