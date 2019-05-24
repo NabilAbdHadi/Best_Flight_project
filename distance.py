7a1c42be2286814865987397a0d041942ac03302
NabiAbdHadi <nabil.hadi69@gmail.com>
2019-05-25 03:18:46 +0800

    the function to calculate the distance between two cities

--- distance.py
+++ distance.py
@@ -1,5 +1,12 @@
 import gmplot
 from geopy import *
+import requests
+import json
+import googlemaps
+import math
+import numpy as np
+from itertools import tee
+
 
 def coordinate(input):
     major_city_address = {}
@@ -9,17 +16,42 @@ def coordinate(input):
         major_city_address[city] = [location.latitude, location.longitude]
     return major_city_address
 
-#plot map using gmplot
+
+# plot map using gmplot
 def plotMap(input):
-    for key,  value in input.items():
+    for key, value in input.items():
         print(key, value)
         gmap = gmplot.GoogleMapPlotter(value[0], value[1], 18)
-        gmap.draw("map%s.html" %key)
+        gmap.draw("map%s.html" % key)
+
+
+def distance(input):
+    disMat = {}
+    """gmaps = googlemaps.Client(key='AIzaSyBYAQcMFPMWlwctq82tQkqoB_-X_TdbprA')  # something wrong
+    for i, Origin in enumerate(input):
+        for j, Destination in enumerate(input):
+            my_dist = gmaps.distance_matrix(Origin, Destination)['rows'][0]['elements'][0]
+            print(i, j)
+            print(my_dist)"""
+    print(disMat)
+    for keyO, valueO in input.items():
+        disMat[keyO] = []
+        for keyD, valueD in input.items():
+            x = valueD[0] - valueO[0]
+            y = valueD[1] - valueO[1]
+            my_dist = math.sqrt(math.pow(x, 2) + math.pow(y, 2))
+            disMat[keyO].append(my_dist)
+            print(my_dist)
+
+    return disMat
+
 
 # Kuala lumpur as the reference place
 major_city = ["Kuala Lumpur", "New York City", "Istanbul", "Beijing", "Manila", "Berlin"]
 
 coor = coordinate(major_city)
-
-plotMap(coor)
-
+print(coor)
+Dis = distance(coor)
+print(Dis)
+# plotMap(coor)
+# distanceCalc(coor)
